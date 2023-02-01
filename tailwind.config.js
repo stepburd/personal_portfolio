4a5ab2f2aa59e9f4dbd90bcad98a443b2ace7f0c
stphnbrdn <58711922+stphnbrdn@users.noreply.github.com>
2023-02-01 09:43:44 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+module.exports = {
+  content: [
+    "./src/index_mock.html"
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        honey : '#EC9706',
+        fire: '#DD571C',
+        slate_grey:"#2B4E47",
+        rust:"#A74B1C",
+        sunray:"#E4A94B",
+        pewter_blue:"#7F9DA8",
+        clay_red:"#B76160",
+        bistre:"#3E2308",
+        khaki_web:"#C6B299",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"]
+      },
+      backgroundImage: {
+
+      }
+    },
+  },
+  plugins: [],
+}
